7bb14f645df3b91ff74396d6f95ecb436839099e
ivan <ivanilson.ferreira.mec@gmail.com>
2022-04-25 04:50:21 -0300

    signup route posting

--- Tweetero-back/index.js
+++ Tweetero-back/index.js
@@ -1,6 +1,28 @@
-import express from "express";
+import express, { json } from "express";
+import cors from "cors";
 
 const app = express();
+app.use(json());
+app.use(cors());
+
+const users = [];
+
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    users.push(req.body);
+    console.log(users)
+    res.send("ok");
+})
+
+app.get("/tweets", (req, res) => {
+    res.send("ok");
+});
+
+app.get("/sign-up", (req, res) => {
+    res.send("ok");
+})
+
 
 app.listen(5000, () => {console.log('Servidor iniciado em http://localhost:5000')});
 
